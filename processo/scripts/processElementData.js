7e5f76012d7ed61536e4b502351191d842ce3c2c
DNascto <dgn.nascto@gmail.com>
2019-07-04 01:44:53 +0000

    Commit do processo

---
+++ processo/scripts/processElementData.js
@@ -0,0 +1,37 @@
+//------------------------------------------------------------------------------
+// Copyright (c) 2005, 2006 IBM Corporation and others.
+// All rights reserved. This program and the accompanying materials
+// are made available under the terms of the Eclipse Public License v1.0
+// which accompanies this distribution, and is available at
+// http://www.eclipse.org/legal/epl-v10.html
+// 
+// Contributors:
+// IBM Corporation - initial implementation
+//------------------------------------------------------------------------------
+
+// This file will include dynamically generated process layout related data from 
+// the publishing service 
+contentPage.processPage.imageFiles["_PpY4YVpwEem4kpa_vSSaqgActivity"]="./../../Meu estoque/deliveryprocesses/resources/Gerenciamento de Estoque Final_8B9EC6E9_8b9ec6e9_Activity.jpeg"
+contentPage.processPage.elementUrls["_bKRhq1pwEem4kpa_vSSaqg"] = ["Validar Estrutura análitica do Projeto","Meu estoque/deliveryprocesses/validar_estrutura_anlitica_do_projeto_653985D0.html"];
+contentPage.processPage.elementUrls["_bKRhulpwEem4kpa_vSSaqg"] = ["Protótipo","Meu estoque/deliveryprocesses/prototipo_EA73887.html"];
+contentPage.processPage.elementUrls["_bKRhp1pwEem4kpa_vSSaqg"] = ["Realizar manutenção e melhorias","Meu estoque/deliveryprocesses/realizar_manuteno_e_melhorias_A0A7A3B1.html"];
+contentPage.processPage.elementUrls["_bKRhrlpwEem4kpa_vSSaqg"] = ["Gerente","Meu estoque/deliveryprocesses/gerente_C0F1922A.html"];
+contentPage.processPage.elementUrls["_PpY4YVpwEem4kpa_vSSaqg"] = ["Gerenciamento de Estoque Final","Meu estoque/deliveryprocesses/Gerenciamento de Estoque Final_8B9EC6E9.html"];
+contentPage.processPage.elementUrls["_bKRhpVpwEem4kpa_vSSaqg"] = ["Entregar Produto","Meu estoque/deliveryprocesses/entregar_produto_4934B9D6.html"];
+contentPage.processPage.elementUrls["_bKRhsFpwEem4kpa_vSSaqg"] = ["Analista","Meu estoque/deliveryprocesses/analista_8C4C8023.html"];
+contentPage.processPage.elementUrls["_bKRhoVpwEem4kpa_vSSaqg"] = ["Definir Escopo do Projeto","Meu estoque/deliveryprocesses/definir_escopo_do_projeto_84A2D7B7.html"];
+contentPage.processPage.elementUrls["_bKRhqlpwEem4kpa_vSSaqg"] = ["Validar Escopo do Projeto","Meu estoque/deliveryprocesses/validar_escopo_do_projeto_FC5FB00B.html"];
+contentPage.processPage.elementUrls["_bKRhqFpwEem4kpa_vSSaqg"] = ["Realizar Triagem","Meu estoque/deliveryprocesses/realizar_triagem_328BBE5.html"];
+contentPage.processPage.elementUrls["_bKRho1pwEem4kpa_vSSaqg"] = ["Desligar o Produto","Meu estoque/deliveryprocesses/desligar_o_produto_DC15C192.html"];
+contentPage.processPage.elementUrls["_bKRhrFpwEem4kpa_vSSaqg"] = ["Validar Requisitos","Meu estoque/deliveryprocesses/validar_requisitos_C7BA9E04.html"];
+contentPage.processPage.elementUrls["_bKRhtlpwEem4kpa_vSSaqg"] = ["EAP","Meu estoque/deliveryprocesses/eap_4A155668.html"];
+contentPage.processPage.elementUrls["_bKRhrVpwEem4kpa_vSSaqg"] = ["Validar Viabilidade do Projeto","Meu estoque/deliveryprocesses/validar_viabilidade_do_projeto_D2587E14.html"];
+contentPage.processPage.elementUrls["_bKRhqVpwEem4kpa_vSSaqg"] = ["Testar","Meu estoque/deliveryprocesses/testar_DC69BF5.html"];
+contentPage.processPage.elementUrls["_bKRhvFpwEem4kpa_vSSaqg"] = ["Requisitos Funcionais e não funcionais","Meu estoque/deliveryprocesses/requisitos_funcionais_nao_funcionais_DA022680.html"];
+contentPage.processPage.elementUrls["_bKRhplpwEem4kpa_vSSaqg"] = ["Implementar","Meu estoque/deliveryprocesses/implementar_37CDCDEC.html"];
+contentPage.processPage.elementUrls["_bKRhpFpwEem4kpa_vSSaqg"] = ["Elicitar Requisitos","Meu estoque/deliveryprocesses/elicitar_requisitos_3E96D9C6.html"];
+contentPage.processPage.elementUrls["_bKRhuFpwEem4kpa_vSSaqg"] = ["Escopo do Projote","Meu estoque/deliveryprocesses/escopo_do_projote_15704461.html"];
+contentPage.processPage.elementUrls["_bKRhtFpwEem4kpa_vSSaqg"] = ["Testador","Meu estoque/deliveryprocesses/testador_50DE6242.html"];
+contentPage.processPage.elementUrls["_bKRholpwEem4kpa_vSSaqg"] = ["Definir viabilidade do projeto","Meu estoque/deliveryprocesses/definir_viabilidade_do_projeto_733BEBCD.html"];
+contentPage.processPage.elementUrls["_bKRhoFpwEem4kpa_vSSaqg"] = ["Criar Estrutura análitica do Projeto","Meu estoque/deliveryprocesses/criar_estrutura_anlitica_do_projeto_7A04F7A7.html"];
+contentPage.processPage.elementUrls["_bKRhslpwEem4kpa_vSSaqg"] = ["Desenvolvedor","Meu estoque/deliveryprocesses/desenvolvedor_85837449.html"];
